8174d32d0866c62fb6b7a5d8b03971bd27adde7c
ali <alidaher9197@gmail.com>
2025-09-03 21:05:50 +0300

    initializing

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="html_icon.png" href="assets/html_icon.png" class="title-logo">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
